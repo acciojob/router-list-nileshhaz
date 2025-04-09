2db45a945707acafae18abc60fe36d6414a283bd
coldBlood <85475503+nileshhaz@users.noreply.github.com>
2025-04-09 15:21:54 +0530

    Create Home.js

---
+++ src/components/Home.js
@@ -0,0 +1,16 @@
+import { Link } from "react-router-dom";
+
+function Home() {
+  return (
+    <>
+      <h1>Item List</h1>
+      <ul>
+        <li><Link to="/items/1">Item 1</Link></li>
+        <li><Link to="/items/2">Item 2</Link></li>
+        <li><Link to="/items/3">Item 3</Link></li>
+      </ul>
+    </>
+  );
+}
+
+export default Home;
